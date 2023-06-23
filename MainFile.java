d89f28470b154f93fff105cbbbb36daedbc20be0
RealChad <113157705+RealChad@users.noreply.github.com>
2023-06-23 20:41:46 +0800

    Added my file and main project file

---
+++ MainFile.java
@@ -0,0 +1,7 @@
+public class MainFile
+{
+    public static void main(String[] args)
+    {
+        //file to submit for MCO1. Do not edit
+    }
+}
---
+++ RobeeFile.java
@@ -0,0 +1,7 @@
+public class RobeeFile 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Test");
+    }
+}
